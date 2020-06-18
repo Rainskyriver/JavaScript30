49920d5f88a1c9679c1272e5e8d3ec99016d6bc0
Rainskyriver <lianyelich14@gmail.com>
2020-06-18 14:57:50 -0700

    Add play bar, End of Day Eleven

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -12,20 +12,26 @@ const togglePlay = () => video.paused ? video.play() : video.pause();
 function toggleIcon() {
     const icon = this.paused ? '►' : '❚ ❚';
     playButton.textContent = icon;
-}
+};
 function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
-}
+};
 function handleRangeUpdate() {
     video[this.name] = this.value;
-}
+};
 function handleProgress() {
-
+    const percent = (video.currentTime / video.duration) * 100;
+    progressF.style.flexBasis = `${percent}%`;
+};
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
 }
 // Event Listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', toggleIcon);
 video.addEventListener('pause', toggleIcon);
+video.addEventListener('timeupdate', handleProgress);
 
 playButton.addEventListener('click', togglePlay);
 
@@ -34,3 +40,8 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
