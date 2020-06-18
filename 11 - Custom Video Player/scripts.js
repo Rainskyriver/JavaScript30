9451b41bd3cf0f7f3d24c5813456ba104adb37f3
Rainskyriver <lianyelich14@gmail.com>
2020-06-18 14:42:00 -0700

    Add play/pause button functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -8,5 +8,16 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 // Build Functions
-
+const togglePlay = () => video.paused ? video.play() : video.pause();
+function toggleIcon() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    playButton.textContent = icon;
+}
+function skip() {
+    
+}
 // Event Listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', toggleIcon);
+video.addEventListener('pause', toggleIcon);
+playButton.addEventListener('click', togglePlay);
